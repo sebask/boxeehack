b7204a19a6300c27ef5ac4dc7045dab6bc3b6dbc
cigamit <jimmy@sqmail.org>
2013-09-05 00:20:16 -0500

    Cache thumbnails to Removable Card
    This checks every removable card on bootup for a directory named
    "cache".  If found, it will auto create the directory structure for each
    profile and setup the mount points for the cache.

--- hack/boot.sh
+++ hack/boot.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sh /data/hack/skin.sh &
+sh /data/hack/cache.sh &
 sh /data/hack/splash.sh &
 sh /data/hack/visualiser.sh &
 sh /data/hack/subtitles.sh &
---
+++ hack/cache.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+found=0
+for m in /media/*; do
+	if [[ -d ${m}/cache ]] && [[ ${found} -eq 0 ]]; then
+		found=1
+		for p in /data/.boxee/UserData/profiles/*; do
+			b=`basename ${p}`
+			if [ ! -d ${m}/cache/${b}/Thumbnails ]; then
+				echo "Creating Cache"
+				mkdir -p ${m}/cache/${b}/Thumbnails
+				mkdir -p ${m}/cache/${b}/Thumbnails/Music
+				mkdir -p ${m}/cache/${b}/Thumbnails/Pictures
+				mkdir -p ${m}/cache/${b}/Thumbnails/Programs
+				mkdir -p ${m}/cache/${b}/Thumbnails/Video
+			fi
+			if [ -d ${m}/cache/${b}/Thumbnails ]; then
+				rm -fr ${p}/Thumbnails/Video/*
+				mount -o bind ${m}/cache/${b}/Thumbnails ${p}/Thumbnails
+			fi
+		done
+		
+	fi
+done
