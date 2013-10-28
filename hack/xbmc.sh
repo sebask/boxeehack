7676049e4cf0fb2490093dd2ba30eb9300596ee1
cigamit <cigamit@boxeed.in>
2013-10-28 13:22:08 -0500

    Add -p flag and ensure that xbmc.bin is executable

--- hack/xbmc.sh
+++ hack/xbmc.sh
@@ -4,7 +4,8 @@ do
 	for m in /tmp/mnt/*; do
         	if [ -f ${m}/xbmc.bin ]; then
 			cd ${m}
-			HOME=${m} GCONV_PATH=${m}/gconv AE_ENGINE=active PYTHONPATH=${m}/python2.7:${m}/python2.7/lib-dynload PYTHONHOME=${m}/python2.7:${m}/python2.7/lib-dynload XBMC_HOME=${m} ${m}/xbmc.bin --standalone 2>>/tmp/xbmc.log
+			chmod +x ${m}/xbmc.bin
+			HOME=${m} GCONV_PATH=${m}/gconv AE_ENGINE=active PYTHONPATH=${m}/python2.7:${m}/python2.7/lib-dynload PYTHONHOME=${m}/python2.7:${m}/python2.7/lib-dynload XBMC_HOME=${m} ${m}/xbmc.bin --standalone -p 2>>/tmp/xbmc.log
 			ret=$?
 			break
 		fi
