ab374c059e4fe50d5710db1a478dabe6b5e9a565
cigamit <cigamit@boxeed.in>
2014-03-28 11:08:54 -0500

    Add misc directory

--- hack/boot.sh
+++ hack/boot.sh
@@ -2,9 +2,11 @@
 # Ensure everything can start
 dos2unix /data/hack/*.sh
 dos2unix /data/hack/init.d/*.sh
+dos2unix /data/hack/misc/*.sh
 chmod 777 /data/hack/*.sh
 chmod 777 /data/hack/bin/*
 chmod 777 /data/hack/init.d/*
+chmod 777 /data/hack/misc/*.sh
 
 find /data/hack/init.d -type f | sort | while read line
 do
